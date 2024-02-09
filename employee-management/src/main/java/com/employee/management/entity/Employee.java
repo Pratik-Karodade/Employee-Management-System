a8ccf333128d02432cc1b893e89f0727d6148e2a
Pratik-Karodade <karodadepratik@gmail.com>
2024-02-09 23:21:06 +0530

    Get all students api create successfully.

--- employee-management/src/main/java/com/employee/management/entity/Employee.java
+++ employee-management/src/main/java/com/employee/management/entity/Employee.java
@@ -1,8 +1,16 @@
 package com.employee.management.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
     @Id
