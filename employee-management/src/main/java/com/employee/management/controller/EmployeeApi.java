07db88faca22f5def8efdf6abd30f909c45ee5cb
Pratik-Karodade <karodadepratik@gmail.com>
2024-02-10 11:14:41 +0530

    Get Put Post Delete Apis for Employee created.

--- employee-management/src/main/java/com/employee/management/controller/EmployeeApi.java
+++ employee-management/src/main/java/com/employee/management/controller/EmployeeApi.java
@@ -5,9 +5,7 @@ import com.employee.management.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +23,7 @@ public class EmployeeApi {
 
     // http://localhost:8080/allStudents
     @GetMapping("/allStudents")
-    public ResponseEntity<List<Employee>> getAllStudents(){
+    public ResponseEntity<List<Employee>> getAllEmployees(){
         List<Employee> employeeList = employeeService.getAllEmployee();
         return new ResponseEntity<>(employeeList, HttpStatus.OK);
     }
@@ -36,4 +34,24 @@ public class EmployeeApi {
        return new ResponseEntity<>(employeeService.getEmployeeById(id),HttpStatus.OK);
     }
 
+    // http://localhost:8080/insertEmployee
+    @PostMapping("/insertEmployee")
+    public ResponseEntity<Employee> insertEmployee(@RequestBody Employee employee){
+        Employee employee1 = employeeService.insertEmployee(employee);
+       return new ResponseEntity<>(employee1,HttpStatus.OK);
+    }
+
+    // http://localhost:8080/deleteEmployee/{id}
+    @DeleteMapping("/deleteEmployee/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>("Successfully deleted employee",HttpStatus.OK);
+    }
+
+    // http://localhost:8080/updateEmployee/{id}
+    @PutMapping("/updateEmployee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        Employee employee1 = employeeService.updateEmployee(id,employee);
+        return new ResponseEntity<>(employee1,HttpStatus.OK);
+    }
 }
--- employee-management/src/main/java/com/employee/management/service/EmployeeService.java
+++ employee-management/src/main/java/com/employee/management/service/EmployeeService.java
@@ -20,4 +20,28 @@ public class EmployeeService {
     public Employee getEmployeeById(Long id){
         return employeeRepo.findById(id).orElse(null);
     }
+
+    public Employee insertEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteById(id);
+    }
+
+    public Employee updateEmployee(Long id, Employee newEmployee) {
+        Employee employee = employeeRepo.findById(id).orElse(null);
+        if (employee != null) {
+            setNewEmployee(employee,newEmployee);
+            return employeeRepo.save(employee);
+        } else {
+            return null;
+        }
+    }
+
+    private void setNewEmployee(Employee employee,Employee newEmployee){
+        employee.setFirstName(newEmployee.getFirstName());
+        employee.setLastName(newEmployee.getLastName());
+        employee.setMobileNum(newEmployee.getMobileNum());
+    }
 }
