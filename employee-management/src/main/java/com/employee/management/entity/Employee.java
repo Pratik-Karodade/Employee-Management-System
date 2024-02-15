8ac4368ea31faa9af364c0b98fcfed4cd5aa3442
Pratik-Karodade <karodadepratik@gmail.com>
2024-02-15 21:56:08 +0530

    Junit test for api

--- employee-management/src/main/java/com/employee/management/entity/Employee.java
+++ employee-management/src/main/java/com/employee/management/entity/Employee.java
@@ -1,16 +1,14 @@
 package com.employee.management.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Employee {
 
     @Id
--- employee-management/src/test/java/com/employee/management/EmployeeApiTest/TestEmployeeApi.java
+++ employee-management/src/test/java/com/employee/management/EmployeeApiTest/TestEmployeeApi.java
@@ -2,11 +2,17 @@ package com.employee.management.EmployeeApiTest;
 
 import com.employee.management.controller.EmployeeApi;
 import com.employee.management.entity.Employee;
+import com.employee.management.service.EmployeeService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootTest
@@ -15,10 +21,22 @@ public class TestEmployeeApi {
     @Autowired
     EmployeeApi employeeApi;
 
+    @MockBean
+    EmployeeService employeeService;
+
     @Test
     void testDemo(){
         String demoOutput = employeeApi.demo();
         Assertions.assertEquals("Welcome to Employee Management System", demoOutput);
+    }
+
+    @Test
+    void testGetAllEmployees(){
+        List<Employee> employeeList = Collections
+                .singletonList(new Employee(1L,"pratik1","karodade","860019321"));
+        Mockito.when(employeeService.getAllEmployee()).thenReturn(employeeList);
+        ResponseEntity<List<Employee>> responseEntity = employeeApi.getAllEmployees();
+        Assertions.assertEquals("860019321",responseEntity.getBody().get(0).getMobileNum());
     }
 
 }
